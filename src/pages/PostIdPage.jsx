4ff6f5caa4538331c5a361c311de30d011eb65a6
sokolovofficial2023 <dev@tararoutray.com>
2023-11-30 21:17:42 +0300

    add-page-posts

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
+import PostService from "../API/PostService";
+import {useFetching} from "../hooks/useFetching";
+import MyLoader from "../components/UI/loader/MyLoader";
 
 const PostIdPage = () => {
     const params = useParams()
+    const [post, setPost] = useState({})
+    const [fetchPostById, isLoading, error] = useFetching(async ()=>{
+            const response = await PostService.getById(params.id)
+            setPost(response.data)
+    })
+    console.log(post)
+    useEffect(() => {
+        fetchPostById(params.id)
+    }, []);
+
     return (
         <div>
-            <h1>Страница поста с ID = ${params.id}</h1>
+            <h1>Пост № {params.id}</h1>
+            {isLoading
+                ? <MyLoader/>
+                : <div>{post.title} {post.body}</div>
+            }
+
         </div>
     );
 };
